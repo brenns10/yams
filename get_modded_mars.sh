d63e2e20aa2cf29ddc56ec24c345b57430a065e4
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-03-19 17:26:41 -0400

    Added a script to create the modified version of MARS, an initial patch containing the Socket Syscalls, and an example in MIPS.

---
+++ get_modded_mars.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+SYSCALL_DIR=mars/mips/instructions/syscalls
+TOUCHED_FILE=__asdf.txt   # double underscore for extra security
+MARS_DIR=mars-sockets
+JAR_NAME=Mars4_5.jar
+PATCH_NAME=mars-sockets.patch
+
+touch $TOUCHED_FILE
+rm -rf $MARS_DIR
+mkdir -p $MARS_DIR
+cd $MARS_DIR
+wget http://courses.missouristate.edu/KenVollmar/MARS/MARS_4_5_Aug2014/Mars4_5.jar
+jar xf $JAR_NAME
+rm -f $JAR_NAME
+patch -p1 < ../$PATCH_NAME
+cd ..
+
+for java_file in $(find $MARS_DIR/$SYSCALL_DIR -type f -newer $TOUCHED_FILE); do
+    javac -cp $MARS_DIR $java_file
+done
+
+rm -f $TOUCHED_FILE
