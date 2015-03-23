f43b8d35329d508f744cf60252fee2c6669694d9
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-23 17:14:13 -0400

    Added File Output Example
    This the file syscall example provided by the MARS developers with some edits.

---
+++ mips/file-o-example.asm
@@ -0,0 +1,24 @@
+        .data
+fout:	   .asciiz "testout.txt"      # filename for output
+buffer:	 .asciiz "The quick brown fox jumps over the lazy dog."
+	.text
+	
+	# Open (for writing) a file that does not exist
+	li   $v0, 13       # system call for open file
+	la   $a0, fout     # output file name
+	li   $a1, 1        # Open for writing (flags are 0: read, 1: write)
+	li   $a2, 0        # mode is ignored
+	syscall            # open a file (file descriptor returned in $v0)
+	move $s6, $v0      # save the file descriptor
+	
+	# Write to file just opened
+	li   $v0, 15       # system call for write to file
+	move $a0, $s6      # file descriptor
+	la   $a1, buffer   # address of buffer from which to write
+	li   $a2, 44       # hardcoded buffer length
+	syscall            # write to file
+	
+	# Close the file
+	li   $v0, 16       # system call for close file
+	move $a0, $s6      # file descriptor to close
+	syscall            # close file
