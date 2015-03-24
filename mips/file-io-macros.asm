513176c6d9565d3cd3333a172f6ce946c325a876
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-24 07:38:50 -0400

    Initial completion of file-io macros for review
    Ordered macro to follow convention of syscall name, then parameters
    Used material from the file output example to choose the correct move/load commands
    Ensured correct parameter names used in all macros

--- mips/file-io-macros.asm
+++ mips/file-io-macros.asm
@@ -6,28 +6,28 @@
 
 # File Macros
 .macro file_open(%filename, %flags, %mode)
-	la $a0, %filename
-	move $a1, %flags
-	move $a2, %mode
 	li $v0, FILE_OPEN
+	la $a0, %filename
+	li $a1, %flags
+	li $a2, %mode
 	syscall
 .end_macro
 .macro file_write(%filedesc, %buffer_address, %max_write_length)
-	la $a0, %filedesc
-	move $a1, %flags
-	move $a2, %mode
 	li $v0, FILE_WRITE
+	move $a0, %filedesc
+	la $a1, %buffer_address
+	li $a2, %max_write_length
 	syscall
 .end_macro
 .macro file_read(%filedesc, %buffer_address, %max_read_length)
-	la $a0, %filedesc
-	move $a1, %flags
-	move $a2, %mode
 	li $v0, FILE_READ
+	move $a0, %filedesc
+	la $a1, %buffer_address
+	li $a2, %max_read_length
 	syscall
 .end_macro
 .macro file_close(%filedesc)
-	move $a0, %filedesc
 	li $v0, FILE_CLOSE
+	move $a0, %filedesc
 	syscall
 .end_macro
