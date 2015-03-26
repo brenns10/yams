ae579013c7c6e36e46a56d00edf47807d31fe0d4
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-03-26 01:48:41 -0400

    Fixed two off-by-one errors that caused the request URI and method names to be clipped.

--- mips/http-requests.asm
+++ mips/http-requests.asm
@@ -9,11 +9,16 @@
 .eqv	HTTP_OTHER	2
 .eqv	HTTP_ERROR	3
 
-.eqv	REQ_METHOD_BUFF_MAX	8
-.eqv	REQ_URI_BUFF_MAX	512
+.eqv	REQ_METHOD_BUFF_MAX	9
+.eqv	REQ_URI_BUFF_MAX	513
 .eqv	REQ_BUFF_MAX		8192
 
+.eqv	req_method_len		$s7
+.eqv	req_uri_len		$s6
+
 .data
+_gr_msg1: .asciiz	"$s7 = <len of method> = "
+_gr_msg2: .asciiz	"$s6 = <len of URI> = "
 chr_space:
 	.asciiz " "
 double_CRLF:
@@ -41,64 +46,68 @@ get_request:
 	push($s5)
 	push($s6)
 	push($s7)
+	push($ra)
 
 	# read the request into the buffer
 	la $a1, req_buff
-	li $a2, REQ_METHOD_BUFF_MAX
+	li $a2, REQ_BUFF_MAX
 	sock_read($s1)
 	
-	# print buffer for debugging
-	print(req_buff)
-	print(ln)
+	# print whole request for debugging
+	#print(req_buff)
+	#print(ln)
 
 	# parsing the status line
-	# get index of the first space
+	# make $s7 = length of request method
 	la $a0, req_buff
-	lb $a1, chr_space
+	lbu $a1, chr_space
 	jal str_index_of
-	move $s7, $v0
-	bltz $s7, req_error	# -1 return --> character not found
+	move req_method_len, $v0
+	bltz req_method_len, _get_request_error	# -1 return --> character not found
+	
+	# make $s6 = length of request uri
+	la $a0, req_buff
+	addi $a0, $a0, 1
+	add $a0, $a0, $s7
+	lbu $a1, chr_space
+	jal str_index_of
+	move req_uri_len, $v0
+	bltz req_uri_len, _get_request_error	# -1 return --> character not found
+	
+	# print out length of request method, uri for debugging
+	#print(_gr_msg1)
+	#print_int(req_method_len)
+	#print(ln)
+	#print(_gr_msg2)
+	#print_int(req_uri_len)
+	#print(ln)
 	
 	# copy the request method
 	la $a0, req_method_buff
 	la $a1, req_buff
-	addi $a2, $s7, -1
+	addi $a2, req_method_len, 1  # add 1 to make space for null terminator
 	li $t0, REQ_METHOD_BUFF_MAX
-	bge $a2, $t0, _copy_req_method
+	bge $t0, $a2, _copy_req_method
 	move $a2, $t0
 _copy_req_method:
-	push($ra)
 	jal strncpy
-	pop($ra)
-	
-	# get index of the second space: start search at first space + 1
-	la $t0, 1(req_buff)
-	add $a0, $t0, $s7
-	lb $a1, chr_space
-	jal str_index_of
-	move $s6, $v0
-	bltz $s6, req_error	# -1 return --> character not found
 	
 	# copy the request uri
 	la $a0, req_uri_buff
-	la $t0, 1(req_buff)
-	add $a1, $t0, $s7
-	sub $a2, $s6, $s7
-	addi $a2, $a2, -1
-	li $t0, REQ_METHOD_BUFF_MAX
-	bge $a2, $t0, _copy_req_uri
+	la $t0, req_buff
+	addi $t1, req_method_len, 1
+	add $a1, $t0, $t1
+	addi $a2, req_uri_len, 1   # add 1 to make space for null terminator
+	li $t0, REQ_URI_BUFF_MAX
+	bge $t0, $a2, _copy_req_uri
 	move $a2, $t0
 _copy_req_uri:
-	push($ra)
 	jal strncpy
-	pop($ra)
 
 	# see if it's a GET request
 	la $a0, req_method_buff
 	la $a1, str_GET
-	push($ra)
 	jal strcmp
-	pop($ra)
 	move $s5, $v0
 
 	li $v0, HTTP_GET
@@ -108,8 +117,9 @@ _copy_req_uri:
 	j _get_request_error
 
 _get_request_error:
-	move $v0, HTTP_ERROR	
+	li $v0, HTTP_ERROR	
 _get_request_return:
+	pop($ra)
 	pop($s7)
 	pop($s6)
 	pop($s5)
--- mips/main.asm
+++ mips/main.asm
@@ -13,6 +13,9 @@
 .eqv	MAX_LEN		16384
 
 .data
+msg0:	.asciiz		"Request Method Type: "
+msg1:	.asciiz		"Request Method (string): "
+msg2:	.asciiz		"Request URI: "
 hwhtml:	.ascii		"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 73\r\nConnection: close\r\n\r\n<html><h1>Hello, world!</h1><br/><h4>Served by MIPS and MARS.</h4></html>" 
 hwhtml_end:
 buff:	.byte	        0:MAX_LEN
@@ -25,10 +28,6 @@ main:
 	ssock_open($s0)		# open server_socket on 19001 and store FD in $s0
 req_loop:
 	ssock_accept($s0, $s1)	# accept connection from server_socket in $s0, store client FD in $s1
-
-	# debug prints!
-	print_int($v1)
-	print(ln)
 	
 	# read & parse a single request
 	move $a0, $s1
@@ -37,7 +36,12 @@ req_loop:
 	move $s2, $v0	# request type, one of HTTP_GET, HTTP_POST, HTTP_OTHER, HTTP_ERROR
 	move $s3, $v1	# req_uri addr
 	
-	print_int($v0)
+	print(msg0)
+	print_int($s2)
+	print(ln)
+
+	print(msg2)
+	print_reg($s3)
 	print(ln)
 
 	# dispatch the appropriate handler -- below is only one for now
@@ -56,4 +60,5 @@ req_loop:
 
 # module includes
 .include "http-requests.asm"
+.include "string.asm"
 
