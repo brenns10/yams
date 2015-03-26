eead3ede8cccf4dd6a18bd403aa3a41948b4992d
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-25 22:33:42 -0400

    Inital I/O Example with Macros, Macro Corrections
    Created an initial file I/O example that uses the macros.
    Fixed the macros to operate correctly for this example. Reading requires a constant max reading length and writing requires a register containing the write length. Corrected the syscall values for file read/write, which were swapped.

--- mips/file-io-macros.asm
+++ mips/file-io-macros.asm
@@ -1,8 +1,16 @@
 # File syscall values
 .eqv	FILE_OPEN	13
-.eqv	FILE_WRITE	14
-.eqv	FILE_READ	15
+.eqv	FILE_READ	14
+.eqv	FILE_WRITE	15
 .eqv	FILE_CLOSE	16
+
+# File opening flags
+.eqv	FILE_OPEN_READ	0
+.eqv	FILE_OPEN_WRITE	1
+.eqv	FILE_OPEN_WRITE_APPEND	9
+
+# Since the file-open syscall ignores mode
+.eqv	FILE_NULL_MODE	0
 
 # File Macros
 .macro file_open(%filename, %flags, %mode, %result_reg)
@@ -13,19 +21,19 @@
 	syscall
 	move %result_reg, $v0
 .end_macro
-.macro file_write(%filedesc, %buffer_address, %max_write_length, %result_reg)
-	li $v0, FILE_WRITE
-	move $a0, %filedesc
-	la $a1, %buffer_address
-	li $a2, %max_write_length
-	syscall
-	move %result_reg, $v0
-.end_macro
 .macro file_read(%filedesc, %buffer_address, %max_read_length, %result_reg)
 	li $v0, FILE_READ
 	move $a0, %filedesc
 	la $a1, %buffer_address
-	li $a2, %max_read_length
+	la $a2, %max_read_length
+	syscall
+	move %result_reg, $v0
+.end_macro
+.macro file_write(%filedesc, %buffer_address, %max_write_length, %result_reg)
+	li $v0, FILE_WRITE
+	move $a0, %filedesc
+	la $a1, %buffer_address
+	move $a2, %max_write_length
 	syscall
 	move %result_reg, $v0
 .end_macro
---
+++ mips/file-io-with-macros-example.asm
@@ -0,0 +1,61 @@
+.include "file-io-macros.asm"
+.include "util-macros.asm"
+
+.eqv MAX_LEN 16384
+
+	.data
+# Output filename
+write_out_name: .asciiz "macro_output.txt~"
+
+write_out_data: .asciiz "HTTP/1.1 200 OK\r\nContent-Type: text/html;\n"
+write_out_data_end:
+
+read_in_name:	.asciiz	"Makefile"
+
+buff: .byte 0:MAX_LEN
+
+newline:	.asciiz	"\n"
+bytes_written:	.asciiz	"Bytes Written: "
+bytes_read:	.asciiz	"Bytes Read: "
+
+	.text
+.globl	main
+main:
+	# Open the file with name in write_out_name
+	# Save descriptor value in $s0 
+	file_open(write_out_name, FILE_OPEN_WRITE, FILE_NULL_MODE, $s0)
+	
+	# Determine length of data to write in $t0
+	la $t0, write_out_data
+	la $t1, write_out_data_end
+	sub $t0, $t1, $t0
+	
+	# Write out to file in $s0 the data in the buffer
+	# Save chars written count in $s1
+	file_write($s0, write_out_data, $t0, $s1)
+	
+	print(bytes_written)
+	move $a0, $s1
+	print_int  # Print the chars written count
+	print(newline)
+	
+	# Close the file descriptor in $s0
+	file_close($s0)
+	
+	# Open the file with name in read_in_name
+	# Save descriptor value in $s0 
+	file_open(read_in_name, FILE_OPEN_READ, FILE_NULL_MODE, $s0)
+	
+	# Read from file in $s0 into buff
+	# Save chars read in $s1
+	file_read($s0, buff, MAX_LEN, $s1)
+	
+	print(bytes_read)
+	move $a0, $s1
+	print_int  # Print the chars read count
+	print(newline)
+	
+	# Close the file descriptor in $s0
+	file_close($s0)
+	
+	exit(0)
