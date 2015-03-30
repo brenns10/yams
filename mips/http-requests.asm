76b74858bc3938c18cac692b1d9e2524bac83c1c
Jeff Copeland Jr <jeffrey.copeland.jr@gmail.com>
2015-03-29 22:48:38 -0400

    HTTP request parser can now handle POST requests. Outlined the logic for receiving such a request.

--- mips/http-requests.asm
+++ mips/http-requests.asm
@@ -11,10 +11,14 @@
 
 .eqv	REQ_METHOD_BUFF_MAX	9
 .eqv	REQ_URI_BUFF_MAX	513
-.eqv	REQ_BUFF_MAX		8192
+.eqv	REQ_BUFF_MAX		1048576	# This is the largest amount of data we can reasonably hold
+.eqv	READ_SIZE		4096
 
 .eqv	req_method_len		$s7
 .eqv	req_uri_len		$s6
+
+# Request buffer management
+#.eqv	req_buff		$s5
 
 .data
 _gr_msg1: .asciiz	"$s7 = <len of method> = "
@@ -32,13 +36,13 @@ str_POST:
 
 req_buff:
 	.byte	0:REQ_BUFF_MAX
-	.byte	0
 req_method_buff:
 	.byte	0:REQ_METHOD_BUFF_MAX
-	.byte	0
 req_uri_buff:
 	.byte	0:REQ_URI_BUFF_MAX
-	.byte	0
+
+req_buff_allocated:
+	.word	0	# set to one once the request buffer is allocated
 
 .text
 get_request:
@@ -47,15 +51,23 @@ get_request:
 	push($s6)
 	push($s7)
 	push($ra)
-
+	
 	# read the request into the buffer
 	la $a1, req_buff
-	li $a2, REQ_BUFF_MAX
+	li $a2, READ_SIZE
 	sock_read($s1)
 	
 	# print whole request for debugging
-	#print(req_buff)
-	#print(ln)
+	print(req_buff)
+	print(ln)
+	
+	
+	# confirm we got at least the header (i.e. check for doubleCRLF)
+	# if we did not get the whole header, drop this request on the floor
+	# check for Transfer-Encoding
+	# check for content length
+	# if neither, we can stop reading
+	
 
 	# parsing the status line
 	# make $s7 = length of request method
@@ -75,12 +87,12 @@ get_request:
 	bltz req_uri_len, _get_request_error	# -1 return --> character not found
 	
 	# print out length of request method, uri for debugging
-	#print(_gr_msg1)
-	#print_int(req_method_len)
-	#print(ln)
-	#print(_gr_msg2)
-	#print_int(req_uri_len)
-	#print(ln)
+	print(_gr_msg1)
+	print_int(req_method_len)
+	print(ln)
+	print(_gr_msg2)
+	print_int(req_uri_len)
+	print(ln)
 	
 	# copy the request method
 	la $a0, req_method_buff
@@ -110,12 +122,23 @@ _copy_req_uri:
 	jal strcmp
 	move $s5, $v0
 
+	# Return the URI if it's a GET request
 	li $v0, HTTP_GET
 	la $v1, req_uri_buff
-	# For now, if it's not a GET request, we throw an error
 	beqz  $s5, _get_request_return
+	
+	# see if it's a POST request
+	la $a0, req_method_buff
+	la $a1, str_POST
+	jal strcmp
+	move $s5, $v0
+	
+	# Return the body if it's a POST request
+	li $v0, HTTP_POST
+	la $v1, req_buff
+	beqz  $s5, _get_request_return
+	
 	j _get_request_error
-
 _get_request_error:
 	li $v0, HTTP_ERROR	
 _get_request_return:
@@ -125,4 +148,3 @@ _get_request_return:
 	pop($s5)
 	pop($s1)
 	jr $ra
-	
