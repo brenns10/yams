eef5238c98bc1674bd04f03068a9eadf61153ab5
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-04-21 17:05:43 -0400

    Initial Configurable file-config Implementation
    ** Not tested!
    Created a separate config file to hold the configuration parameters for all features being configured.
    Created a (copied) version of file.asm as file-config.asm that doesn't have critical constants included in it. These constants must be supplied in the config.

---
+++ mips/config.asm
@@ -0,0 +1,9 @@
+###
+# File: config.asm
+#
+# Configuration file for all options being configured in the YAMS instance
+###
+
+# Config fields for file-config.asm
+_file__file_default_dir:	.asciiz "html"
+_file_default_resource:	.asciiz "index.html"
