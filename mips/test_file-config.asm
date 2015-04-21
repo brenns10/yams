9b0eef8112e045bf33a879c18a3d9ce98007b77f
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-04-21 17:13:46 -0400

    Copied test_file to test_file-config and Implemented Configurable Files
    Have the configuration .include'd in the upper portion of the .data section. I am unsure if this placement is required.

---
+++ mips/test_file-config.asm
@@ -0,0 +1,71 @@
+###
+# File: test_file.asm
+#
+# Code to exercise the URI file handler implementation.
+###
+
+.include "util-macros.asm"
+.include "file-io-macros.asm"
+
+.data
+test_pass:	.asciiz "TEST PASSED.\n"
+test_fail:	.asciiz "==> TEST FAILED!\n"
+test_start:	.asciiz "STARTING TESTS.\n"
+test_end:	.asciiz "FINISHED TESTS.\n"
+
+test_uri1:	.asciiz "/"
+test_uri2:	.asciiz "/minimal_page.html"
+test_uri3:	.asciiz "/invalid/invalid.txt"
+
+.include "config.asm"
+
+.text
+.globl main
+main:
+	print(test_start)
+	
+	jal test_root_uri
+	jal test_specific_resource
+	jal test_inaccessible_resource
+	
+	print(test_end)
+	exit(0)
+
+# Utility jump points for tests
+pass:
+	print(test_pass)
+	jr $ra
+fail:
+	print(test_fail)
+	jr $ra
+
+## Testing uri_file_handle_fetch
+
+test_root_uri:
+	push($ra)
+	la $a0, test_uri1
+	jal uri_file_handle_fetch
+	pop($ra)
+	
+	blt $v0, $zero, fail  # Expect to get a proper file handle
+	j pass
+
+test_specific_resource:
+	push($ra)
+	la $a0, test_uri2
+	jal uri_file_handle_fetch
+	pop($ra)
+	
+	blt $v0, $zero, fail  # Expect to get a proper file handle
+	j pass
+
+test_inaccessible_resource:
+	push($ra)
+	la $a0, test_uri3
+	jal uri_file_handle_fetch
+	pop($ra)
+	
+	bge $v0, $zero, fail   # Expect to get an invalid (<0) file handle
+	j pass
+
+.include "file-config.asm"
